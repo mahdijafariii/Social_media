8230c12cbb2e87a20daf41ebf1c65d07f72d710d
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-15 00:10:34 +0330

    config mongo db

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const app = require('./app');
-
-
+const {default : mongoose} = require('mongoose')
+const e = require("express");
+require('dotenv').config()
 function startServer (){
-    app.listen(4002,()=>{
-        console.log("server is running on port 4002 !");
+    app.listen(process.env.PORT,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("Server is running on port 4002 !");
+        }
     })
+}
+
+function connectToDb(){
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log('Db is Connected!'));
 }
 function run(){
     startServer();
+    connectToDb()
 }
 
 run();
