7cedfa16409110f7ac846ecbeeff37fff4080973
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-14 22:50:09 +0330

    add index css file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// static folders
+app.use('/css',express.static(path.join(__dirname,'public/css')));
+app.use('/fonts',express.static(path.join(__dirname,'public/fonts')))
+app.use('/js',express.static(path.join(__dirname,'public/js')));
+app.use('/images',express.static(path.join(__dirname,'public/images')))
+
 // ejs
 app.set("view engine", "ejs");
 app.set("ejs", path.join(__dirname , "views"))
