497df823fbcfdd553867799e3321aa58bddc4e93
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-14 23:24:27 +0330

    first page ejs is complete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require('path');
+const path = require("path");
 const app = express();
 
+app.use(express.urlencoded({extended : true , limit : "50mb"}));
+app.use(express.json({limit : "50mb"}));
+
 // static folders
+app.use(express.static(path.join(__dirname,"..","public")))
 app.use('/css',express.static(path.join(__dirname,'public/css')));
 app.use('/fonts',express.static(path.join(__dirname,'public/fonts')))
 app.use('/js',express.static(path.join(__dirname,'public/js')));
@@ -10,12 +14,15 @@ app.use('/images',express.static(path.join(__dirname,'public/images')))
 
 // ejs
 app.set("view engine", "ejs");
-app.set("ejs", path.join(__dirname , "views"))
-
+app.set("views", path.join(__dirname , "views"))
 
 // router
 app.get("/",(req ,res)=>{
     return res.render("index")
+})
+
+app.use((req,res)=>{
+    console.log("this path is not found :" , req.path);
 })
 
 module.exports = app;
