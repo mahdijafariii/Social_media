71eee1813ac65511a7b17d27ce1212d755529e98
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-15 22:47:15 +0330

    add register validator

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,9 +1,32 @@
 const userModel = require('../../models/User');
-
+const {errorResponses, successResponses} = require("../../utils/responses");
+const {registerValidationSchema} = require("./auth.validator");
 
 const register = async (req,res)=>{
-    const {password , name , email , username , biography} = req.body;
+    const {password , name , email , username } = req.body;
 
+    await registerValidationSchema.validate({email,name,username,password},{
+        abortEarly : false
+    });
+
+    const isExistUser = await userModel.findOne({username}).lean();
+    if(isExistUser){
+        return errorResponses(res,400,"This user already exist !!")
+    }
+
+    const isFirstUser = await userModel.countDocuments() === 0;
+    let role = "USER";
+    if(isFirstUser){
+        role = "ADMIN";
+    }
+
+    const user = new userModel({email,password,name,username});
+    await user.save();
+
+    return successResponses(res,201,{
+        message : "User added successfully !!",
+        user : user
+    })
 
 
 };
---
+++ src/modules/auth/auth.validator.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+exports.registerValidationSchema = yup.object({
+    email: yup
+        .string()
+        .email("Please enter a valid email")
+        .required("Email is required"),
+    username: yup
+        .string()
+        .min(3, "Username must be at least 3 chars long")
+        .required("Username is required"),
+    name: yup
+        .string()
+        .min(3, "Name must be at least 3 chars long")
+        .max(50, "Name cannot be more than 50 chars long")
+        .required("Name is required"),
+    // .optional(),
+
+    password: yup
+        .string()
+        .min(8, "Password must be at least 8 chars long")
+        .required("Password is required"),
+});
