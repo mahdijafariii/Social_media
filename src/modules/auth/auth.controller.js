2f7776066e1a59b92b27ea8c32e466b937fdbb4e
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-10-15 22:17:12 +0330

    add user module contoller and router

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,11 @@
+const userModel = require('../../models/User');
+
+
+const register = async (req,res)=>{
+    const {password , name , email , username , biography} = req.body;
+
+
+
+};
+
+module.exports = {register};
---
+++ src/modules/auth/auth.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userController = require('../../modules/auth/auth.controller');
+
+
+
+
+const router = express.Router();
+module.exports = router;
